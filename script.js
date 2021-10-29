ec077a44970cdfcce73149ef1d1b401f0dc47c2a
Vignesh Karkera <vigneshkarkera32@gmail.com>
2021-10-29 08:01:28 +0530

    name change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 (function(){
-  var names = ["Vignesh", "Jaison", "John", "Joker", "Prajwal", "Luffy", "Naruto", "Goku", "Itachi", "jack"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
 
       var firstLetter = names[i].charAt(0).toLowerCase();
